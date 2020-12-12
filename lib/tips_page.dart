4e6396117b6a761cc43438f19c939b5f1e535c34
kulloveth <kulloveth@gmail.com>
2020-12-12 19:42:03 +0100

    create total tips container

--- lib/tips_page.dart
+++ lib/tips_page.dart
@@ -10,24 +10,33 @@ class TipsCalculator extends StatefulWidget {
 }
 
 class _TipsCalculatorState extends State<TipsCalculator> {
+  final double tip = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title),
-
       ),
       body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Container(
-
-              ),
-            )
-          ],
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue.shade300),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.only(top: 20.0,left: 40.0,right: 40.0),
+                child: Column(
+                  children: [Text('Total Per Person'), Text('\$ $tip')],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
